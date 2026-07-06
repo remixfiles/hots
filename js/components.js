ab6dd5c54ac6ab18f9014424b9c0fa72141dd599
Toyobur PCHome <enambm11@gmail.com>
2026-07-06 19:17:28 +0600

    after head tag ad code

--- js/components.js
+++ js/components.js
@@ -149,11 +149,3 @@ function copyToClipboard(text) {
 // Expose for article pages if needed
 window.HOTS = { copyToClipboard };
 
-// JS Load Monetag Ads 
-self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 11246817
-}
-self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
-
